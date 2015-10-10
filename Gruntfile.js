4973ef85741d9c33c0b5367c565f080960bb1c0a
sd-forge <sd@weareforge.co>
2015-10-10 12:15:33 +1100

    move location of twreplace.js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
 			dist: {
+				options: {
+					banner: '<%= tag.banner %>'
+				},
 				files: {
 					'css/app.css' : 'scss/app.scss'
 				}
@@ -13,6 +16,16 @@ module.exports = function(grunt) {
 				files: '**/*.scss',
 				tasks: ['sass']
 			}
+		},
+		tag: {
+			banner: '/*!\n' +
+				    ' * <%= pkg.name %>\n' +
+				    ' * <%= pkg.description %>\n' +
+				    ' * <%= pkg.homepage %>\n' +
+				    ' * @author <%= pkg.author %>\n' +
+				    ' * @version <%= pkg.version %>\n' +
+				    ' * Copyright 2015. <%= pkg.license %> licensed.\n' +
+				    ' */\n'
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
