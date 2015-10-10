a9466ee7e2f51a337bb23e5b9ea8a0c70ede32e9
sd-forge <sd@weareforge.co>
2015-10-10 12:23:57 +1100

    fix some errors with files not appearing

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,6 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
 			dist: {
-				options: {
-					banner: '<%= tag.banner %>'
-				},
 				files: {
 					'css/app.css' : 'scss/app.scss'
 				}
@@ -16,16 +13,6 @@ module.exports = function(grunt) {
 				files: '**/*.scss',
 				tasks: ['sass']
 			}
-		},
-		tag: {
-			banner: '/*!\n' +
-				    ' * <%= pkg.name %>\n' +
-				    ' * <%= pkg.description %>\n' +
-				    ' * <%= pkg.homepage %>\n' +
-				    ' * @author <%= pkg.author %>\n' +
-				    ' * @version <%= pkg.version %>\n' +
-				    ' * Copyright 2015. <%= pkg.license %> licensed.\n' +
-				    ' */\n'
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
