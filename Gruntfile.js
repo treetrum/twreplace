17c0adcdbe4c1995a1f6570514410e24b6e86da7
sd-forge <sd@weareforge.co>
2015-10-15 20:26:55 +1100

    add a minfied version

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,14 +8,29 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+		    },
+			my_target: {
+				files: {
+					'dist/twreplace.min.js': ['src/twreplace.js']
+				}
+			}
+		},
 		watch: {
 			css: {
 				files: '**/*.scss',
 				tasks: ['sass']
+			},
+			js: {
+				files: 'src/*.js',
+				tasks: ['uglify']
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.registerTask('default',['watch']);
 }
